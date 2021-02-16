73c9afa186662fa5677e9aeca06979cb192c36ba
Hazedom <78011849+Hazedom@users.noreply.github.com>
2021-02-16 21:30:25 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Mathelulw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Möglichkeiten = { "A", "B", "J", "K"};
+
+            foreach (string P in Möglichkeiten)
+            {
+                foreach (string S in Möglichkeiten)
+
+
+                Console.Write("(" + P + "," + S + ")"+",");
+
+            }
+
+        }
+    }
+}
